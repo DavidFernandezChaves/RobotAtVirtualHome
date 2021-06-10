2e00450afd1f09a9186f8343faa3ff84e4e9a2de
josematez <josematez@uma.es>
2021-06-10 16:56:09 +0200

    2.0.4

--- Assets/RobotAtVirtualHome/Scripts/Utils/SmartCamera.cs
+++ Assets/RobotAtVirtualHome/Scripts/Utils/SmartCamera.cs
@@ -67,7 +67,7 @@ public class SmartCamera : MonoBehaviour
 
     public void Disconnected(ROS ros) {
         if (sendImagesToROS) {
-            StopAllCoroutines();
+            StopCoroutine(SendImages(ros));
         }
     }
 
