132f8e9f847a02c5800f493c368ce0eb311b412f
josematez <josematez@uma.es>
2021-06-14 19:46:30 +0200

    2.0.4

--- Assets/RobotAtVirtualHome/Scripts/ScriptTest.cs
+++ Assets/RobotAtVirtualHome/Scripts/ScriptTest.cs
@@ -7,15 +7,22 @@ public class ScriptTest : MonoBehaviour
 
     public float angle = 0;
     public List<Transform> transforms;
+    public List<Transform> transforms2;
 
     private void OnDrawGizmos() {
 
         List<Vector3> points = new List<Vector3>();
+        float distance = 0;
 
-        foreach(Transform t in transforms) {
-            Gizmos.DrawSphere(Quaternion.Euler(0, -angle, 0) * t.position, 0.1f);
+        for(int i = 0; i < transforms.Count; i++)
+        {
+            distance += Vector3.Distance(transforms[i].position, transforms2[i].position);
+            Gizmos.color = Color.white;
+            Gizmos.DrawSphere(transforms[i].position, 0.1f);
+            Gizmos.color = Color.white;
+            Gizmos.DrawSphere(transforms2[i].position, 0.1f);
         }
 
-
+        Debug.Log(distance);
     }
 }
