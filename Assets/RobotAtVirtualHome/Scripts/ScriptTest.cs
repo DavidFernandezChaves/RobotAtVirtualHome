1290f6f881ad00262219cc703bdcc5832bee95a5
josematez <josematez@uma.es>
2021-06-15 12:23:41 +0200

    2.0.4

--- Assets/RobotAtVirtualHome/Scripts/ScriptTest.cs
+++ Assets/RobotAtVirtualHome/Scripts/ScriptTest.cs
@@ -6,23 +6,25 @@ public class ScriptTest : MonoBehaviour
 {
 
     public float angle = 0;
-    public List<Transform> transforms;
-    public List<Transform> transforms2;
+    public VirtualObjectBox vob1;
+    public VirtualObjectBox vob2;
 
     private void OnDrawGizmos() {
-
-        List<Vector3> points = new List<Vector3>();
-        float distance = 0;
-
-        for(int i = 0; i < transforms.Count; i++)
+        if (vob1 != null && vob2 != null)
         {
-            distance += Vector3.Distance(transforms[i].position, transforms2[i].position);
-            Gizmos.color = Color.white;
-            Gizmos.DrawSphere(transforms[i].position, 0.1f);
-            Gizmos.color = Color.white;
-            Gizmos.DrawSphere(transforms2[i].position, 0.1f);
+            List<SemanticObject.Corner> ref1 = vob1.semanticObject.Corners;
+            List<SemanticObject.Corner> obs = vob2.semanticObject.Corners;
+            float distance = 0;
+
+            for (int i = 0; i < ref1.Count; i++)
+            {
+                Gizmos.DrawLine(ref1[i].position, obs[i].position);
+            }
+
+            Debug.Log(VirtualObjectSystem.CalculateCornerDistance(ref1, obs, false));
+            vob1 = null;
+            vob2 = null;
         }
 
-        Debug.Log(distance);
     }
 }
